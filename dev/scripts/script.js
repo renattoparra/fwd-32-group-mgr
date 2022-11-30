12d01ab9fbcc0ad43b714f785ae779a1a81d4eee
MiRim Yu <firepunch119@gmail.com>
2022-11-30 15:05:34 -0800

    refactor script.js

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,40 +1,35 @@
 $(".hero").click(function () {
-  $('#hamburger').removeClass("is-active");
-  $('.drop-down-menu').removeClass("is-active")
+  $("#hamburger").removeClass("is-active");
+  $(".drop-down-menu").removeClass("is-active");
 });
 
 $("#hamburger").click(function () {
   $(this).toggleClass("is-active");
-  $('.drop-down-menu').toggleClass("is-active")
+  $(".drop-down-menu").toggleClass("is-active");
 });
 
-$(".c").click(function () {
-  let clas = $(this).attr('class');
-  console.log(clas);
-  let number;
-  if (clas === "c c1") { number = 1 }
-  else if (clas === "c c2") { number = 2 }
-  else if (clas === "c c3") { number = 3 }
-  $('.hero').css("background-image", `url(../images/header-slider-img-0${number}.jpg)`)
+$(".dot").click(function () {
+  const targetDotClass = $(this).attr('class');
+  const dotNumber = targetDotClass.charAt(8);
+
+  $(".dot").removeClass("dot-active");
+  $(this).toggleClass("dot-active");
+  $(".hero").css("background-image", `url(../images/header-slider-img-0${dotNumber}.jpg)`);
 });
 
-background(1);
+changeBackground(0);
 
-function background(x) {
+function changeBackground(dotNumber) {
   setInterval(() => {
-    console.log('s');
-    if (x > 2) x = 0;
-    let backgroundA = document.querySelector('.hero');
-    let dots = document.querySelectorAll('.c');
-    dots.forEach((item, index) => {
-      // item.style.background = 'white';
-      // if(index==(x-1))item.style.background='blue';
+    ++dotNumber;
 
-      const color = index === x ? '#429bd6' : "white";
-      item.style.background = color;
-    })
+    if (dotNumber > 3) {
+      dotNumber = 0;
+      return;
+    }
 
-    backgroundA.style.backgroundImage = `url(../images/header-slider-img-0${x + 1}.jpg)`;
-    x++;
-  }, 3000)
+    $(".dot").removeClass("dot-active");
+    $(`.dot-${dotNumber}`).toggleClass("dot-active");
+    $(".hero").css("background-image", `url(../images/header-slider-img-0${dotNumber}.jpg)`);
+  }, 3000);
 }
